b2bf434c736f08ca8b7b52bf51d6a07bf9704ca1
jafrul-islam <jafrulstc@gmail.com>
2024-12-22 23:15:34 +0600

    product add

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 import Header from "~/components/header/header";
+import Products from "~/components/products/products";
 
 export default component$(() => {
   return (
     <>
       <Header />
+      <Products />
     </>
   );
 });
