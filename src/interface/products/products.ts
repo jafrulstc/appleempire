d4bcb7dc393f15db2383e239ef075b31f24f07ca
jafrul-islam <jafrulstc@gmail.com>
2024-12-22 23:02:45 +0600

    header add

--- src/interface/products/products.ts
+++
@@ -1,9 +0,0 @@
-export default interface ProductsType {
-    name: string,
-    brand: string,
-    model: string,
-    category: string
-    description: string,
-    image: string,
-    price: number,
-}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-import Footer from "~/components/footer/footer";
 import Header from "~/components/header/header";
-import Products from "~/components/products/products";
-
 
 export default component$(() => {
   return (
     <>
       <Header />
-      <Products />
-      <Footer />
     </>
   );
 });
